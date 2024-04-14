7c0bc0ba5a8f1c501fbc061ed3ed9db07c09446b
Sensie21 <mafaaz.sabir@gmail.com>
2024-04-14 19:51:22 +0530

    Blogs now are accessed from database

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const {engine}  = require('express-handlebars');
-const dbConn = require('./db')
 
-const dbConnect = dbConn.dbConnect
-const getDb = dbConn.getDb
 
 const path = require('path');
 
@@ -14,22 +11,16 @@ app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
-dbConnect((err) =>{
-  if(!err){
-    app.listen(port, () => {
-      console.log(`Example app listening on port ${port}`);
-    });
-  }
-  else{
-    console.log(err + `Connection`);
-  }
-})
 
-// Serve static files
-app.use(express.static(path.join(__dirname, 'static')));
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+    // Define routes
+const blogRoutes = require(path.join(__dirname, 'routes', 'blog.js'))
+app.use('/', blogRoutes)
 
-// Define routes
-app.use('/', require(path.join(__dirname, 'routes', 'blog.js')));
+
+
 
 
 
--- routes/blog.js
+++ routes/blog.js
@@ -1,41 +1,93 @@
 const express = require('express')
 const router = express.Router()
-const path = require('path')
-const blgs = require('../data/blogs.js')
-const res = require('express/lib/response.js')
+// const blgs = require('../data/blogs.js')
+const dbConn = require('./db')
 
-const app = express()
+const dbConnect = dbConn.dbConnect
+const getDb = dbConn.getDb
 
-app.use(express.json())
+let db
+
+dbConnect((err) => {
+    if(!err){
+        console.log("Successful Connection")
+    }
+    db = getDb()
+})
+
+function createSlug(title) {
+    return title.toLowerCase().replace(/[^a-zA-Z0-9 ]/g, '').replace(/\s+/g, '-');
+}
+
+router.use(express.urlencoded({ extended: false }));
 
 router.get("/", (req,res) => {
     res.render('home')
 })
-
+//BlogHomePage
 router.get("/blog", (req,res) => {
-    res.render('BlogHome',{
-        blog: blgs
-    })
+    let blgs = []
+    db.collection('blog')
+        .find()
+        .sort({author : 1})
+        .forEach(blg => {blgs.push(blg)})
+        .then(() =>{
+            res.status(200)
+            res.render('BlogHome',{
+                blog: blgs
+            })
+            console.log(blgs)
+        })
+        .catch(()=>{
+            res.status(500)
+        })
+    
 })
-
+//Display a Single Blog
 router.get("/blog/:slug", (req,res) => {
-    myBlog = blgs.filter((e)=>{
-        return e.slug == req.params.slug
-    })
-    res.render('eachBlog',{
-        title: myBlog[0].name,
-        author: myBlog[0].author,
-        content: myBlog[0].content
-    })
+    db.collection('blog')
+        .findOne({slug : req.params.slug})
+
+        .then(doc =>{
+            res.status(200)
+            res.render('eachBlog',{
+                title: doc.name,
+                author : doc.author,
+                content : doc.content
+            })
+            console.log(blgs)
+        })
+        .catch(()=>{
+            res.status(500)
+        })
 })
 
+
+//Add a blog
 router.get("/addBlog",(req,res) =>{
     res.render('addBlog')
 })
 
-router.post('/articles',(req,res) =>{
-    let data = req.body
-    console.log(data)
+router.post('/addBlog',(req,res) =>{
+    if (!req.body || !req.body.title) {
+        return res.status(400).send('Title is missing in the request body');
+    }
+    const { title,content } = req.body;
+    const currentDate = new Date().toISOString().split('T')[0];
+    const author = 'Mafaaz'
+    const slug = createSlug(title)
+    db.collection('blog')
+        .insertOne({"name" : title,"author" : author,"date" : currentDate,"content" : content,"slug" : slug})
+        .then(doc =>{
+            res.status(201)
+            console.log("Successful Insertion")
+        })
+        .catch(err =>{
+                res.status(500)
+                console.log("Error In Insertion")
+            }
+        )
+    console.log(title,content)
     res.redirect('/blog')
 })
 
